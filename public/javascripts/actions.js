1557e037bf5678d3f97bc483ef4b81d16c5180fd
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-05-24 16:36:00 +0100

    added zero button

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -52,4 +52,26 @@ $(function() {
 			});
 		}
 	})
+	$('.zero').click(function(e) {
+		var btn = $(e.target)
+		console.log(btn.attr('data-id'))
+		console.log(btn.attr('data-times'))
+		var data = {
+			id : btn.attr('data-id'),
+			timesBought : 0,
+			date: Date.now()
+		}
+		console.log(data)
+		var url = "/update"
+		$.ajax({
+			type : 'POST',
+			url : url,
+			data : data
+		}).done(function() {
+			location.reload();
+		}).error(function(e) {
+			console.log(e)
+		});
+	})
+
 });
